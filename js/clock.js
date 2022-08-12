8a1013fce57c36603a399ae2a261fb2d80b05e41
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-08-12 16:51:29 +0900

    Re: create clock.js

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clock = document.querySelector("#time");
+
+function mainClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}`;
+}
+
+mainClock();
+setInterval(mainClock, 1000);
