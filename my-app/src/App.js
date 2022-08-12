d36c90a670576a267b05122fc700d5edcb1271d9
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-08-12 18:58:49 +0900

    Create Login.js
    index.html->Login.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./routes/Login";
+import "./styles.css";
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="main">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+        <Routes>
+          <Route path="/home" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ my-app/src/routes/Login.js
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import "./Login.module.css";
+import styles from "./Login.module.css";
+
+function Login() {
+  return (
+    <div>
+      <header className={styles.welcome_header}>
+        <img
+          className={styles.welcome_header__img}
+          src="img/cocoa.jpg"
+          alt="#"
+        />
+        <h1 className={styles.welcome_header__title}>Welcome to Cocoa Clone</h1>
+        <p className={styles.welcome_header__text}>
+          If you have a Cocoa Account, log in with your email or phone number.
+        </p>
+      </header>
+
+      <form method="get" className={styles.login_form}>
+        <input
+          name="username"
+          className={styles.login_form__input}
+          type="text"
+          placeholder="Email or phone number"
+        />
+        <input
+          name="password"
+          className={styles.login_form__input}
+          type="password"
+          placeholder="Password"
+        />
+        <Link to="/home">
+          <input
+            className={styles.login_form__btn}
+            type="submit"
+            value="Log In"
+          />
+        </Link>
+        <span className={styles.login_form__a}>
+          Find Cocoa Account or Password
+        </span>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
